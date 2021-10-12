ce6c62aa8d86f395b450e87c04c70b66b5a07ddb
Paul Beffa <paulbeffa@outlook.fr>
2021-10-12 17:20:45 +0200

    Add login view

---
+++ Shared/MiscellaneousUtilities.swift
@@ -0,0 +1,29 @@
+//
+//  MiscellaneousUtilities.swift
+//  SpotifyManager
+//
+//  Created by Paul Beffa on 14/09/2021.
+//
+
+import Foundation
+import SwiftUI
+import SpotifyWebAPI
+
+extension View {
+    
+    /// Type erases self to `AnyView`. Equivalent to `AnyView(self)`.
+    func eraseToAnyView() -> AnyView {
+        return AnyView(self)
+    }
+
+}
+
+extension ProcessInfo {
+    
+    /// Whether or not this process is running within the context of a SwiftUI
+    /// preview.
+    var isPreviewing: Bool {
+        return self.environment["XCODE_RUNNING_FOR_PREVIEWS"] == "1"
+    }
+
+}
